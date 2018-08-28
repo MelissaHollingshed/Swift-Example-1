a497a60f6a1340a8db8b8e5ecdabb5f98c3cd7a0
Melissa Hollingshed <melissa@mwc-091058.dhcp.missouri.edu>
2018-08-28 14:54:41 -0500

    Applied gitignore

---
+++ Swift Example 1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Melissa Hollingshed on 8/28/18.
+//  Copyright © 2018 Melissa Hollingshed. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
